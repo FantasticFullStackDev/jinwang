74d2e66ad32a5af117ca71c34b38c4587aa645c9
WebMobileDevelopment <maximmironenkoreactu@example.com>
2022-01-03 09:00:20 +0900

    jinwang 1/3/2022

---
+++ assets/js/custom.js
@@ -0,0 +1,51 @@
+// Script to open and close sidebar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    // document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    // document.getElementById("myOverlay").style.display = "none";
+}
+
+var slideIndex = 1;
+(function () {
+    showDivs(slideIndex);
+})();
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("slides");
+    var p = document.getElementsByClassName("paginations");
+    slideIndex = n;
+    if (n > x.length) {slideIndex = 1;}
+    if (n < 1) {slideIndex = x.length;}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    for (i = 0; i < p.length; i++) {
+        p[i].className = p[i].className.replace("w3-black", "w3-hover-black");  
+    }
+    x[slideIndex-1].style.display = "block";  
+    p[slideIndex-1].className = p[slideIndex-1].className.replace("w3-hover-black", "w3-black");
+    var elem = document.getElementById("portfolio");
+    elem.scrollIntoView({behavior: 'smooth'});
+}
+
+function sendMail() {
+    var name = $("#Name").val();
+    var title = $("#Title").val();
+    var message = $("#Message").val();
+    if(name != "" && title != "" && message != "") {
+        window.open("mailto:jinwangdev531@gmail.com?subject=" + title + "&" + message + "\n" + name);
+    }
+}
